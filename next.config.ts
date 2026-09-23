17254fac5d39e5bd779c7ba58abc01b64e9eb471
Terry Nguyen <terryknguyen@gmail.com>
2026-09-23 12:55:01 -0400

    Set up gh pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.*", "localhost:3000", "127.0.0.1:3000"],
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
